56af169bcc9011c7ac0fd40375cfdafc7dd67008
Pedro-Goncal <bpg.pedro@gmail.com>
2020-10-29 13:21:39 -0600

    On Click Tailer with title rating and synopsis

--- script.js
+++ script.js
@@ -44,13 +44,78 @@ function fetchMovies(url, className, path) {
 }
 
 function showMovies(movies, className, path) {
-  console.log(movies);
-
   const moviesEl = document.querySelector(className);
 
   for (let movie of movies.results) {
-    let image = `<img class="poster" src="https://image.tmdb.org/t/p/original/${movie[path]}"></img>`;
+    let imageElement = document.createElement("img");
+    imageElement.src = `https://image.tmdb.org/t/p/original/${movie[path]}`;
+    imageElement.addEventListener("click", (e) => {
+      handleMovieSelection(e, movie);
+    });
 
-    moviesEl.innerHTML += image;
+    moviesEl.appendChild(imageElement);
   }
 }
+
+/*
+===============Movie Trailer===============
+*/
+
+const handleMovieSelection = (e, movie) => {
+  console.log(movie);
+
+  document.getElementById("rating").innerHTML = `Rating: ${movie.vote_average}`;
+  document.getElementById("overview").innerHTML = movie.overview;
+  document.getElementById("title").innerHTML = movie.original_title;
+
+  getMovieTrailer(movie.id).then((data) => {
+    const results = data.results;
+    const youtubeTrailer = results.filter((results) => {
+      if (results.site == "YouTube" && results.type == "Trailer") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    setTrailer(youtubeTrailer);
+    // console.log(data);
+    // console.log(youtubeTrailer);
+  });
+  $(`#trailerModal`).modal("show");
+};
+
+async function getMovieTrailer(id) {
+  let url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=947b0f0af52c9b74afa43eed2267820d&language=en-US`;
+  return await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Something went wrong");
+    }
+  });
+}
+
+const setTrailer = (trailers) => {
+  const iframe = document.getElementById("movieTrailer");
+  const movieNotFound = document.querySelector(".movie-notFound");
+  const rating = document.getElementById("rating");
+  const overview = document.getElementById("overview");
+  const title = document.getElementById("title");
+
+  if (trailers.length > 0) {
+    iframe.classList.remove("d-none");
+    movieNotFound.classList.add("d-none");
+    rating.classList.remove("d-none");
+    overview.classList.remove("d-none");
+    title.classList.remove("d-none");
+
+    iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
+  } else {
+    iframe.classList.add("d-none");
+    movieNotFound.classList.remove("d-none");
+    rating.classList.add("d-none");
+    overview.classList.add("d-none");
+    title.classList.add("d-none");
+  }
+};
